2a398e91e5291512f12e683569808fc1ffd31ca0
monicafgz <monikafgz@gmail.com>
2020-06-02 08:48:35 +0200

    bg image issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,50 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
+    screens: {
+      sm: '576px',
+      // => @media (min-width: 576px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '992px',
+      // => @media (min-width: 992px) { ... }
+
+      xl: '1220px',
+      // => @media (min-width: 1220px) { ... }
+    },
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.1em',
+      widest: '.2em',
+    },
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      full: '100%',
+      lg: '420px',
+      lx: '500px',
+    },
   },
   variants: {},
   plugins: [],
-}
+  corePlugins: {
+    backgroundAttachment: true,
+  },
+};
